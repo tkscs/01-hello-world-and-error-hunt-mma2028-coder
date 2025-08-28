6fbd51378d582655f48d19446a8288516749ddeb
mma2028-coder <mma2028@kehillahstudent.org>
2025-08-28 09:11:18 -0700

    finished finding errors

---
+++ hello_world.py
@@ -0,0 +1,31 @@
+
+#  File "c:\Users\matth\github-classroom\tkscs\01-hello-world-and-error-hunt-mma2028-coder\hello_world.py", line 1
+#     print("hello, world"
+#          ^
+# # SyntaxError: '(' was never closed
+# # print(helo wordld)
+# #  File "c:\Users\matth\github-classroom\tkscs\01-hello-world-and-error-hunt-mma2028-coder\hello_world.py", line 1
+# #     print("hello, world"
+# #           ^^^^^^^^^^^^^^
+# # SyntaxError: invalid syntax. Perhaps you forgot a comma?
+# # pint("hello, world")
+# File "c:\Users\matth\github-classroom\tkscs\01-hello-world-and-error-hunt-mma2028-coder\hello_world.py", line 1
+#     print("hello, world"
+#          ^
+# SyntaxError: '(' was never closed
+# print(""""helo"""world""":)
+#       File "c:\Users\matth\github-classroom\tkscs\01-hello-world-and-error-hunt-mma2028-coder\hello_world.py", line 17
+#     print(""""helo"""world""":)
+#                           ^
+# SyntaxError: unterminated triple-quoted string literal (detected at line 18)
+# print:(hello, world))
+#   File "c:\Users\matth\github-classroom\tkscs\01-hello-world-and-error-hunt-mma2028-coder\hello_world.py", line 1
+#     print("hello, world"
+#           ^^^^^^^^^^^^^^
+# SyntaxError: invalid syntax. Perhaps you forgot a comma?
+#  File "c:\Users\matth\github-classroom\tkscs\01-hello-world-and-error-hunt-mma2028-coder\hello_world.py", line 26
+#     pint(hloworld)aio31232
+#                   ^^^^^^^^
+# SyntaxError: invalid syntax
+print(2+2)
+print()
